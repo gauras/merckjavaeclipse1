74edd1bf90f5defe3228ecd1d44ae3a5ef5861e2
gkumar <gaurgk947@gamil.com>
2022-01-18 15:45:44 +0530

    first commit

---
+++ src/main/java/merckjavaeclipse1/OneOffProcess.java
@@ -0,0 +1,8 @@
+package merckjavaeclipse1;
+public class OneOffProcess
+{
+    public static void main(String[] args)
+    {
+        System.out.println("OneOffProcess executed.");
+    }    
+}
---
+++ src/main/java/merckjavaeclipse1/WorkerProcess.java
@@ -0,0 +1,14 @@
+package merckjavaeclipse1;
+
+public class WorkerProcess
+{
+    public static void main(String[] args)
+    {
+        while(true) {
+        	try { 
+        	    Thread.sleep(1000);
+            } catch(InterruptedException e) {}
+            System.out.println("Worker process woke up");
+        }
+    }    
+}
